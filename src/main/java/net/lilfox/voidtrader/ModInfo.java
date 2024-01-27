c6c6e3325a3d6ad046a4c917ffd9a5c7e0fe9774
lil-fox <developed.by.fox@gmail.com>
2024-01-27 05:54:42 +0100

    Initial commit

---
+++ src/main/java/net/lilfox/voidtrader/ModInfo.java
@@ -0,0 +1,7 @@
+package net.lilfox.voidtrader;
+
+public class ModInfo {
+
+    public static final String MOD_ID = "voidtrader";
+    public static final String MOD_NAME = "voidTrader";
+}
---
+++ src/main/java/net/lilfox/voidtrader/client/VoidTraderClient.java
@@ -0,0 +1,10 @@
+package net.lilfox.voidtrader.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VoidTraderClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
